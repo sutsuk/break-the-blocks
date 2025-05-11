363c5069968d2b02a485346dc059717e7de83e28
sutsuk <107033259+sutsuk@users.noreply.github.com>
2025-05-12 00:16:42 +0900

    Update eraseBlocks.js

--- src/js/eraseBlocks.js
+++ src/js/eraseBlocks.js
@@ -23,51 +23,51 @@ function eraseLine(lineNum) {
   console.log(`Erase line: ${lineNum}`);
   function searchSameBlock(y, x, type, eraseBlocks) {
     let a;
-    if(type > 0) {
-      for(a = 0; a < eraseBlocks.length; a++) {
-        if(eraseBlocks[a].x == x && eraseBlocks[a].y == y) {
+    let newEraseBlocks = eraseBlocks;
+    if(type >= 0) {
+      for(a = 0; a < newEraseBlocks.length; a++) {
+        if(newEraseBlocks[a].x == x && eraseBlocks[a].y == y) {
           break;
         }
       }
-      if(a == eraseBlocks.length) {
-        eraseBlocks.push({x:x, y:y});
+      if(a == newEraseBlocks.length) {
+        newEraseBlocks.push({x:x, y:y});
         combo++;
         if(y > 0 && newBlockData[y - 1][x] == type) {
-          eraseBlocks = searchSameBlock(y - 1, x, type, eraseBlocks);
+          newEraseBlocks = searchSameBlock(y - 1, x, type, newEraseBlocks);
         }
         if(y < blockDataHeight - 1 && newBlockData[y + 1][x] == type) {
-          eraseBlocks = searchSameBlock(y + 1, x, type, eraseBlocks);
+          newEraseBlocks = searchSameBlock(y + 1, x, type, newEraseBlocks);
         }
         if(x > 0 && newBlockData[y][x - 1] == type) {
-          eraseBlocks = searchSameBlock(y, x - 1, type, eraseBlocks);
+          newEraseBlocks = searchSameBlock(y, x - 1, type, newEraseBlocks);
         }
         if(x < blockDataWidth - 1 && newBlockData[y][x + 1] == type) {
-          eraseBlocks = searchSameBlock(y, x + 1, type, eraseBlocks);
+          newEraseBlocks = searchSameBlock(y, x + 1, type, newEraseBlocks);
         }
       }
     }
-    return eraseBlocks;
+    return newEraseBlocks;
   }
-  let x, a;
-  let eraseBlocks; 
+  let x, y, a;
   let placed; 
   let skipped; 
   let scoreCombo; 
   let combo;
-  let erasedBlocksSameLine = new Array();
+  let erasedBlocksSameLine = [];
   let newBlockData = get_blockData();
-  for(x = 0; x < blockDataWidth; x++) {
-    for(a = 0; a < erasedBlocksSameLine.length; a++) {
-      if(erasedBlocksSameLine[a] == x) {
+  for (x = 0; x < blockDataWidth; x++) {
+    for (a = 0; a < erasedBlocksSameLine.length; a++) {
+      if (erasedBlocksSameLine[a].x == x) {
         break;
       }
     }
-    if(a < erasedBlocksSameLine.length) {
+    if (a < erasedBlocksSameLine.length) {
       continue;
     }
     scoreCombo = 0; 
     combo = 0;
-    eraseBlocks = searchSameBlock(lineNum, x, newBlockData[lineNum][x], []);
+    let eraseBlocks = searchSameBlock(lineNum, x, newBlockData[lineNum][x], []);
     if (eraseBlocks.length < eraseThreshold && eraseBlocks.length > 0) {
       eraseBlocks = [eraseBlocks[0]];
       combo = 1;
@@ -76,30 +76,30 @@ function eraseLine(lineNum) {
       scoreCombo = ((combo - 1) * scoreComboBonus + 1) * scoresPerBlock;
       console.log(`${combo} Combo (+${scoreCombo}pt)`);
     }
-    eraseBlocks.forEach((block) => {
-      newBlockData[block.y][block.x] = -1;
-      if (block.y == lineNum) {
-        erasedBlocksSameLine.push(block.x);
+    for (a = 0; a < eraseBlocks.length; a++) {
+      newBlockData[eraseBlocks[a].y][eraseBlocks[a].x] = -1;
+      if (eraseBlocks[a].y == lineNum) {
+        erasedBlocksSameLine.push(eraseBlocks[a]);
       }
-      console.log(`Erase blockData[${block.y}][${block.x}]`);
-    });
+      console.log(`Erase blockData[${eraseBlocks[a].y}][${eraseBlocks[a].x}]`);
+    }
     console.log(`Total scores = ${scoreTotal += scoreCombo}`);
-    eraseBlocks.forEach((block) => {
-      placed = 0; 
-      skipped = 0;
-      for (a = blockDataHeight - 1; a >= 0; a--) {
-        if (newBlockData[a][block.x] >= 0) {
-          newBlockData[blockDataHeight - 1 - placed++][block.x] = newBlockData[a][block.x];
-        } else {
-          skipped++;
-        }
-      }
-      for(j = 0; j < skipped; j++) {
-        newBlockData[j][block.x] = -1;
-      }
-    });
-    update_blockData(newBlockData);
   }
+  for (x = 0; x < blockDataWidth; x++) {
+    placed = 0; 
+    skipped = 0;
+    for (y = blockDataHeight - 1; y >= 0; y--) {
+      if (newBlockData[y][x] >= 0) {
+        newBlockData[blockDataHeight - 1 - placed++][x] = newBlockData[y][x];
+      } else {
+        skipped++;
+      }
+    }
+    for (y = 0; y < skipped; y++) {
+      newBlockData[y][x] = -1;
+    }
+  }
+  update_blockData(newBlockData);
   searchErasableLine();
 }
 
